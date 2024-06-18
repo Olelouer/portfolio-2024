a34933e97fbceba8abf14cdce0b5bccdbef7a0d2
Olelouer <olivier.lelouer@protonmail.com>
2024-06-17 20:00:44 -0400

    add translations + form + set project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'lib-projects',
   templateUrl: './projects.component.html',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  projectsList: Array<any> = [];
+  filters: Array<string> = [];
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.get('projects').subscribe((res: any) => {
+      this.projectsList = res.projects;
+      this.filters = res.filters;
+    });
+  }
+}
