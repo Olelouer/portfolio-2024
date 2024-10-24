32e5b147613633f40ef285f78ea4155aeb133029
Olelouer <olivier.lelouer@protonmail.com>
2024-10-24 12:21:25 +0200

    mobile version finished

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as focusTrap from 'focus-trap';
 
 @Component({
   selector: 'lib-panel',
   templateUrl: './panel.component.html',
 })
-export class PanelComponent {}
+export class PanelComponent {
+  isOpen = false;
+  private trap: any;
+
+  @ViewChild('panel', { static: false }) panelElement!: ElementRef;
+
+  togglePanel() {
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.activateFocusTrap();
+    } else {
+      this.deactivateFocusTrap();
+    }
+  }
+
+  closePanel() {
+    this.isOpen = false;
+    this.deactivateFocusTrap();
+  }
+
+  activateFocusTrap() {
+    if (this.panelElement) {
+      this.trap = focusTrap.createFocusTrap(this.panelElement.nativeElement, {
+        escapeDeactivates: true,
+        clickOutsideDeactivates: true,
+      });
+      this.trap.activate();
+    }
+  }
+
+  deactivateFocusTrap() {
+    if (this.trap) {
+      this.trap.deactivate();
+    }
+  }
+}
